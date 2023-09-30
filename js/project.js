6253e1adbeaa4043225ea00357e55954b3fc3166
Narsis <narsiskh.2013@gmail.com>
2023-09-30 20:13:12 +0330

    Creating Task Manager with HTML, CSS & JavaScript.

--- js/project.js
+++ js/project.js
@@ -1,20 +1,38 @@
 // Variables
 const task_input = document.querySelector('.task_input');
 const add = document.querySelector('.add_task');
+const task_list = document.querySelector('.task_list');
+const myArray = [];
 
 
+// Removing task_list child one by one
+const taskFunction = () => {
+    while (task_list.firstChild){
+        task_list.removeChild(task_list.firstChild)
+    }
+    //
+    for (let i = 0; i <  myArray.length; i++) {
+        const div = document.createElement('div');
+        const taskName = document.createElement('p');
+        taskName.innerHTML = myArray[i]
+
+        div.append(taskName)
+
+        div.classList.add('item')
+        task_list.append(div)
+    }
+}
+
+// What will happen when you click +
 add.addEventListener('click', function (){
     if(task_input.value.length === 0){
         alert("Please write your task!")
     }else{
-        const div = document.createElement('div');
-        const taskName = document.createElement('p');
-        taskName.innerHTML = task_input.value
+        myArray.push(task_input.value)
 
-        div.append(taskName)
+        task_input.value = ""
 
-        const task_list = document.querySelector('.task_list');
-        div.classList.add('item')
-        task_list.append(div)
+        taskFunction()
     }
 })
+
