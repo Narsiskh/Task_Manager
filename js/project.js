dd06bacfb5786119d621a2ee7f7d0ba43ae8eef4
Narsis <narsiskh.2013@gmail.com>
2023-10-02 19:08:00 +0330

    Edit and delete button is added to project.

--- js/project.js
+++ js/project.js
@@ -10,14 +10,57 @@ const taskFunction = () => {
     while (task_list.firstChild){
         task_list.removeChild(task_list.firstChild)
     }
-    //
+
     for (let i = 0; i <  myArray.length; i++) {
         const div = document.createElement('div');
-        const taskName = document.createElement('p');
-        taskName.innerHTML = myArray[i]
 
-        div.append(taskName)
+        div.classList.add("d-flex", "justify-content-between", "align-items-center", "p-3")
 
+        const taskName = document.createElement('input');
+
+        taskName.disabled = true
+
+        const div1 = document.createElement('div');
+        const deleteBtn = document.createElement('button');
+        const editBtn = document.createElement("button")
+        const submitBtn = document.createElement('button')
+
+        submitBtn.hidden = true
+
+
+        // Delete The Tasks
+        deleteBtn.innerHTML = "<i class=\"bi bi-trash3\"></i>"
+
+        deleteBtn.addEventListener("click", () => {
+            myArray.splice(i, 1)
+            taskFunction()
+        })
+
+        // Edit The Task
+        editBtn.innerHTML = "<i class=\"bi bi-pencil-square\"></i>"
+        editBtn.addEventListener('click', () => {
+            taskName.disabled = false
+            editBtn.hidden = true
+            submitBtn.hidden = false
+        })
+
+        // Submit The Task
+        submitBtn.innerHTML = "<i class=\"bi bi-check-square\"></i>"
+        submitBtn.addEventListener('click', () => {
+            myArray[i] = taskName.value
+            taskFunction()
+        })
+
+
+
+
+
+        taskName.value = myArray[i]
+
+        div1.append(deleteBtn, editBtn, submitBtn)
+        div.append(taskName, div1)
+
+        div.classList.add('item')
         div.classList.add('item')
         task_list.append(div)
     }
@@ -35,4 +78,9 @@ add.addEventListener('click', function (){
         taskFunction()
     }
 })
+
+
+
+
+
 
